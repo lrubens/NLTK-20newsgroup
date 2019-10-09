dd7e0fea6fa2fd0cdf76c2302bbb467b0254fa5d
lrubens <lacouture.r@husky.neu.edu>
2019-10-09 18:03:26 -0400

    pushing wordcloud

--- midterm.py
+++ midterm.py
@@ -1,4 +1,8 @@
 import time
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import json
 import pandas as pd
 import numpy as np
 import warnings
@@ -14,6 +18,7 @@ import matplotlib.pyplot as plt
 import plotly
 import plotly.graph_objs as go
 from bs4 import BeautifulSoup
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -28,24 +33,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 
-def remove_tabs(words):
-    tabs = str.maketrans('', '', '\t');
-    return [word.translate(tabs) for word in words]
-
-
 def remove_non_ascii(words):
     """Remove non-ASCII characters from list of tokenized words"""
     new_words = []
     for word in words:
         new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
         new_words.append(new_word)
-    return new_words
-
-
-def replace_contractions(words):
-    new_words = []
-    for word in words:
-        new_words.append(contractions.fix(word))
     return new_words
 
 
@@ -58,41 +51,10 @@ def to_lowercase(words):
     return new_words
 
 
-def remove_punctuation(words):
-    """Remove punctuation from list of tokenized words"""
-    new_words = []
-    tokenizer = RegexpTokenizer(r'\w+')
-    for word in words:
-        new_word = tokenizer.tokenize(word)
-        new_word = ' '.join(new_word)
-        if new_word != '':
-            new_words.append(new_word)
-    return new_words
-
-
 def remove_emails(words):
     new_words = []
     for word in words:
         new_word = re.sub(r'\S*@\S*\s?', '', word)
-        if new_word != '':
-            new_words.append(new_word)
-    return new_words
-
-
-def remove_newline(words):
-    new_words = []
-    for word in words:
-        new_word = re.sub('\s+', '', word)
-        if new_word != '':
-            new_words.append(new_word)
-    return new_words
-
-
-def remove_numbers(words):
-    """Replace all interger occurrences in list of tokenized words with textual representation"""
-    new_words = []
-    for word in words:
-        new_word = re.sub(r'\d+', '', word)
         if new_word != '':
             new_words.append(new_word)
     return new_words
@@ -104,13 +66,13 @@ def remove_stopwords(words):
     new_words = [i for i in words if not i in stop_words]
     return new_words
 
+def lemmatize_stemming(text):
+    # stemmer = SnowballStemmer('english')
+    stemmer = PorterStemmer()
+    words = ' '.join(text)
+    stems = stemmer.stem(WordNetLemmatizer().lemmatize(words, pos='v'))
+    return gensim.utils.simple_preprocess(stems)
 
-def lemmatize(words):
-    lemmatizer = WordNetLemmatizer()
-    new_words = []
-    for word in words:
-        new_words.append(lemmatizer.lemmatize(word))
-    return new_words
 
 def stem_words(words):
     """Stem words in list of tokenized words"""
@@ -120,7 +82,7 @@ def stem_words(words):
 
 
 def remove_smallwords(words):
-    new_words = [i for i in words if len(i) > 2]
+    new_words = [i for i in words if len(i) > 3]
     return new_words
 
 
@@ -128,57 +90,49 @@ def remove_nonalphabet(words):
     return re.sub('[^A-Za-z]', ' ', words)
 
 
+def remove_nonenglish(words):
+    english_words = set(nltk.corpus.words.words())
+    return [words[i] for i in range(len(words)) if words[i] in english_words or not words[i].isalpha()]
+
+
 def preprocess(words):
     words = remove_non_ascii(words)
-    # words = remove_numbers(words)
     words = to_lowercase(words)
-    words = replace_contractions(words)
-    words = remove_punctuation(words)
     words = remove_stopwords(words)
-    words = remove_tabs(words)
-    words = remove_emails(words)
-    words = lemmatize(words)
-    # words = stem_words(words)
     words = remove_smallwords(words)
+    words = remove_emails(words)
+    # words = remove_nonenglish(words)
+    words = lemmatize_stemming(words)
     return words
 
 
 def get_data():
-    categories = ["comp.windows.x", "comp.os.ms-windows.misc", "talk.politics.misc", "comp.sys.ibm.pc.hardware",
-                  "talk.religion.misc", "rec.autos",
-                  "sci.space", "talk.politics.guns", "alt.atheism", "misc.forsale", "comp.graphics", "sci.electronics",
-                  "sci.crypt", "soc.religion.christian",
-                  "rec.sport.hockey", "sci.med", "rec.motorcycles", "comp.sys.mac.hardware", "talk.politics.mideast",
-                  "rec.sport.baseball"];
-    description = ["Alt - Atheism", "Computer - Graphics", "Computer - Microsoft/Windows", "Computer - IBM/PC",
-                   "Computer - Mac",
-                   "Computer - Windows", "Misc. - For Sale", "Rec. - Autos", "Rec - Motorcycles", "Sports - Baseball",
-                   "Sports - Hockey",
-                   "Science - Cryptography", "Science - Electronics", "Science - Medicine", "Science - Space",
-                   "Religion - Christianity",
-                   "Politics - Guns", "Politics - Middle East", "Politics - Misc.", "Religion - Misc."]
-    raw_dataset = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+    raw_dataset = fetch_20newsgroups(subset='train', categories=["talk.politics.guns", "sci.crypt"], shuffle=True, random_state=42)
+    # raw_dataset = fetch_20newsgroups(subset='train', shuffle=True, random_state=42)
     dataset = raw_dataset.data
-    dataset = ' '.join(dataset)
-    dataset = remove_nonalphabet(dataset)
-    tokens = word_tokenize(dataset)
-    return tokens
+    return dataset
 
 
 def main():
     raw_data = get_data()
-    corpus = preprocess(raw_data)
-    count_vect = CountVectorizer(max_df=0.85, max_features=1000)
-    word_count_vect = count_vect.fit_transform(corpus)
-    print(count_vect.vocabulary_)
-    print(word_count_vect.shape)
-    # print(list(count_vect.vocabulary_.keys())[:10])
-    # tfidf_transformer = TfidfTransformer()
-    # vectors = tfidf_transformer.fit(word_count_vect)
-    # feature_names = vectors
-    # X_train_tfidf.shape
-    # print(corpus)
-    # print(preprocessed_text)
+    corpus = []
+    for line in raw_data:
+        line = remove_nonalphabet(line)
+        tokens = gensim.utils.simple_preprocess(line)
+        data_chunks = preprocess(tokens)
+        corpus.append(data_chunks)
+    dictionary = gensim.corpora.Dictionary(corpus)
+    bow_corpus = [dictionary.doc2bow(doc) for doc in corpus]
+    vocab = list(dictionary.values()) #list of terms in the dictionary
+    vocab_tf = [dict(i) for i in bow_corpus]
+    vocab_tf = list(pd.DataFrame(vocab_tf).sum(axis=0)) #list of term frequencies
+    pairs = [(vocab[i], vocab_tf[i]) for i in range(len(vocab_tf)) if vocab_tf[i] > 2]
+    cnt = Counter()
+    for word, frequency in pairs:
+        cnt[word] = frequency
+    wordcloud = WordCloud(width=800, height=600, relative_scaling=.8).generate_from_frequencies(cnt)
+    wordcloud.to_file("wordcloud.png")
+
 
 if __name__ == "__main__":
     main()
